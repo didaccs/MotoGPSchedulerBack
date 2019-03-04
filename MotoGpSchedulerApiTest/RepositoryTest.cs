e32ce5d5ad12c23d05e47c351f076fab3599fd6d
Didac <didac.campanals.ext@suez.com>
2019-03-04 23:23:15 +0100

    Add repository tests

--- MotoGpSchedulerApiTest/RepositoryTest.cs
+++ MotoGpSchedulerApiTest/RepositoryTest.cs
@@ -19,6 +19,33 @@ namespace MotoGpSchedulerApiTest
 
             Assert.AreEqual(recordsResult.Count, records.Count);
             Assert.AreEqual(recordsResult[0].Pilot, records[0].Pilot);
-        }        
+        }
+
+        [TestMethod]
+        public void GetByNamePilot_Record()
+        {
+            InitializeDatabase();
+            List<Record> records = DbInitializer.GetRecords();
+
+            List<Record> recordsResult = repoRecord.GetAll(r=> r.Pilot == "Jorge Lorenzo").ToList();
+
+            Assert.AreEqual(recordsResult.Count, 4);            
+        }
+
+        [TestMethod]
+        public void Insert_Record()
+        {
+            Record record = new Record()
+            {
+                Pilot = "Marc Márquez",
+                Time = new System.TimeSpan(0, 0, 1, 32, 351)
+            };
+
+            repoRecord.Insert(record);
+            List<Record> recordsResult = repoRecord.GetAll().ToList();
+
+            Assert.AreEqual(recordsResult.Count, 1);
+            Assert.AreEqual(recordsResult[0].Pilot, record.Pilot);
+        }
     }
 }
